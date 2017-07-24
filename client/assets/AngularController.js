45d17638fd91e29b487db22d0c38e8d3259d353b
conniechu929 <conniechu929@gmail.com>
2017-07-23 19:08:13 -0700

    added recaptcha with confirmation for wallet address

--- client/assets/AngularController.js
+++ client/assets/AngularController.js
@@ -1,8 +1,10 @@
 console.log('AngularController is here');
 app.controller('AngularController', ['$scope', '$routeParams', '$rootScope', function($scope, $routeParams, $rootScope) {
+  $scope.key = "6Le5BioUAAAAAKIKpUaEcoVKGJ0aXxQohLns_rUD";
+
   var present_date = new Date().getTime()/1000;
   var sellingdate = new Date('Jul 21, 2017 16:40:00').getTime()/1000;
-  var deadline = new Date("Jul 31, 2017 20:16:00").getTime()/1000;
+  var deadline = new Date("Aug 05, 2017 20:16:00").getTime()/1000;
   var counter;
 
   $(function(){
@@ -32,14 +34,17 @@ app.controller('AngularController', ['$scope', '$routeParams', '$rootScope', fun
       });
     });
 
-    function captcha_callback() {
-      $('.captcha_button').prop("disabled", false);
-      // $('.token_button').show()
+    $scope.verify = function() {
+      console.log("INSIDE VERIFY FUNCTION")
+      console.log(typeof(grecaptcha.getResponse()));
+      console.log($scope.confirm);
+      if(($scope.confirm == true) && (typeof(grecaptcha.getResponse()) === 'string')) {
+        $(".token_address").show();
+        $('#captcha').hide();
+      }
+      else{
+        $('#captcha').show();
+      }
     };
 
-    var captcha_response = recaptcha.getResponse();
-    
-    if (capptcha_response) {
-      $('.token_button').show();
-    }
 }]);
